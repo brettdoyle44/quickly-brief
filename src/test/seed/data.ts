9f66d4c6d8e3ce8dd7960b6b8fe7a49e35d5e67f
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2022-07-26 11:06:20 -0400

    First commit

---
+++ src/test/seed/data.ts
@@ -0,0 +1,20 @@
+import { SeedData } from "./index";
+
+export const testData: SeedData = {
+  users: [
+    {
+      id: "test",
+      name: "Tester",
+      email: "test@test.com",
+    },
+  ],
+  projects: [
+    {
+      id: "test",
+      name: "Test Project",
+      slug: "test",
+      members: ["test"],
+      owner: "test",
+    },
+  ],
+};
